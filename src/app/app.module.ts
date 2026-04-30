d0e632903693ef541ead7cc3e51239613aa8da64
RogerioNeto07 <joserogeriofonsecauzl@gmail.com>
2026-04-29 21:57:50 -0300

    add portifolio screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,36 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NbThemeModule, NbLayoutModule, NbButtonModule } from '@nebular/theme';
+import { 
+  NbThemeModule, 
+  NbLayoutModule, 
+  NbSidebarModule, 
+  NbCardModule, 
+  NbUserModule, 
+  NbIconModule, 
+  NbActionsModule, 
+  NbBadgeModule, 
+  NbButtonModule 
+} from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NbThemeModule.forRoot({ name: 'corporate' }),
+    NbThemeModule.forRoot({ name: 'cosmic' }),
     NbLayoutModule,
-    NbButtonModule
+    NbButtonModule,
+    NbLayoutModule,
+    NbSidebarModule.forRoot(),
+    NbCardModule,
+    NbUserModule,
+    NbIconModule,
+    NbEvaIconsModule,
+    NbActionsModule,
+    NbBadgeModule,
+    NbButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
